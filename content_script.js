42cc4687ce9119c752fce0c58e291bd84241fb0f
zvakanaka <adam.quinton@familysearch.org>
2022-06-27 17:11:52 -0600

    add chrome compat and selectors can go through shadow dom

--- content_script.js
+++ content_script.js
@@ -54,7 +54,40 @@ function findElement(selector) {
     }
   }
 
-  return element
+  return element || shadowSelectorAll(selector)?.[0] // fall back to searching through shadow DOM
+}
+
+// Thank you nathnolt: https://stackoverflow.com/a/71787772/4151489
+function shadowSelectorAll(selector, rootNode = document.body) {
+  const arr = []
+  const traverser = node => {
+      // 1. decline all nodes that are not elements
+      if(node.nodeType !== Node.ELEMENT_NODE) return
+
+      // 2. add the node to the array, if it matches the selector
+      if(node.matches(selector)) arr.push(node)
+
+      // 3. loop through the children
+      const children = node.children
+      if (children.length) {
+          for (const child of children) {
+              traverser(child)
+          }
+      }
+
+      // 4. check for shadow DOM, and loop through it's children
+      const shadowRoot = node.shadowRoot
+      if (shadowRoot) {
+          const shadowChildren = shadowRoot.children
+          for(const shadowChild of shadowChildren) {
+              traverser(shadowChild)
+          }
+      }
+  }
+
+  traverser(rootNode)
+
+  return arr
 }
 
 async function keyListener(e) {
